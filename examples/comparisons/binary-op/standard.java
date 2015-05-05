85fffcba782e9e54a7f3235680308ed1285999e6
Daryl Seah <darylseah@gmail.com>
2015-05-06 01:19:37 +0800

    Fix standard.java for binary-op

--- examples/comparisons/binary-op/standard.java
+++ examples/comparisons/binary-op/standard.java
@@ -1,53 +1,10 @@
-class Main {
-    void main() {
-        Queue q = new Queue();
-        q.add("Hello");
-        q.add("World");
-
-        Iterator i = q.iterator();
-        while (i.hasNext()) {
-            println(i.next());
-        }
+class Num {
+    int value;
+    void add(Num other) {
+        value += other.value;
     }
 }
 
-class Queue {
-    Link first;
-    Link last;
-
-    void add(Object value) {
-        if (last == null) {
-            first = last = new Link(value);
-        } else {
-            last.next = new Link(value);
-            last = last.next;
-        }
-    }
-
-    Object remove() {
-        if (first == null) {
-            return null;
-        }
-        Object value = first.value;
-        first = first.next;
-        if (first == null) {
-            last = null;
-        }
-        return value;
-    }
-
-    void append(Queue q) {
-        for (Link cur = q.first; cur != null; cur = cur.next) {
-            add(cur.value);
-        }
-    }
-}
-
-class Link {
-    Link next;
-    Object value;
-
-    Link(Object value) {
-        this.value = value;
-    }
-}
+Num a = new Num(1);
+Num b = new Num(2);
+a.add(b);
