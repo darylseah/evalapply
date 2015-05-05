bab79552a2cd300fb137a89f0cfa4af28de030fa
Joel Low <joel@joelsplace.sg>
2015-05-05 09:46:30 +0800

    Move another code listing to the examples directory for inclusion.

---
+++ examples/universe-types/generic-universe-types.java
@@ -0,0 +1,34 @@
+class Link<X> {
+	X next;
+}
+
+class Node<K, V> extends Link<peer Node<K, V>> {
+	K key; V value;
+}
+
+class Iterator<X extends any Link<X>> {
+	X current;
+
+	Iterator(X first) {
+		current = first;
+	}
+
+	pure boolean hasNext() {
+		return current != null;
+	}
+
+	impure X next() {
+		X result = current;
+		current = current.next;
+		return result;
+	}
+}
+
+class Map<K, V> {
+	rep Node<K, V> first;
+
+	impure peer Iterator<rep Node<K, V>> iterator() {
+		return new peer Iterator
+				<rep Node<K, V>>(first);
+	}
+}
